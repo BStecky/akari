c39e164ac1885104989cc1c733e4d28f39476fe8
Jordan Freeman <jordansolfreeman@outlook.com>
2022-04-13 22:09:34 -0700

    Navbar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -44,6 +44,7 @@ const Sidebar = ({toggle, menuOpen }) => {
                 <li><Link href="/" className="my-4 transition-all hover:text-yellow-200" passHref>HOME</Link></li>
                 <li><Link href="https://light-city.io" className="my-4 transition-all hover:text-yellow-200">STAKING</Link></li>
                 <li><Link href="https://lp.light-city.io" className="my-4 transition-all hover:text-sitePurple hover:bg-white px-4 py-2 border-[1px] rounded border-white" passHref>GLOW LP</Link></li>
+                <li><Link href="https://shrine.light-city.io/" className="my-4 transition-all hover:text-yellow-200" >THE SHRINE</Link></li>
                     <li><hr className='my-4'/></li>
                     <li>
                         <div className='flex justify-center text-3xl'>
