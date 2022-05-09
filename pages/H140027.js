27efd89da34500f5a50c9410a89288f6c108e25c
Jordan Freeman <jordansolfreeman@outlook.com>
2022-05-08 19:00:38 -0700

    H140027

---
+++ pages/H140027.js
@@ -0,0 +1,13 @@
+import CountdownCard from "../components/Countdown"
+import Box from "../components/Box"
+
+const H140027 = () => {
+    return (
+        <container className='overflow-x-hidden'>
+            <CountdownCard/>
+            <Box/>
+        </container>
+    )
+}
+
+export default H140027
