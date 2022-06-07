1b87220bc631eac9276e074a4da8c47c0bfbff2f
Jordan Freeman <jordansolfreeman@outlook.com>
2022-06-07 15:06:29 -0700

    whatisthis?

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/H140027":["static\u002Fchunks\u002Fpages\u002FH140027.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FH140027","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
