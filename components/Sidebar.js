a6e49823b1d0cb56d3441931c8fb3a439b68a0aa
Jordan Freeman <jordansolfreeman@outlook.com>
2022-06-21 16:23:47 -0700

    New Nav

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,7 +2,6 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { AiOutlineClose } from 'react-icons/ai'
 import { FaDiscord, FaTwitter } from 'react-icons/fa'
 import Link from 'next/link'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Sidebar = ({toggle, menuOpen }) => {
     return (
@@ -27,7 +26,7 @@ const Sidebar = ({toggle, menuOpen }) => {
                 animate='visible'
                 exit='hidden'
             />
-            <motion.nav className='fixed top-0 right-0 h-screen w-screen sm:w-1/2 bg-gray-900 flex-col justify-center items-center text-center p-24'
+            <motion.nav className='fixed top-0 right-0 h-screen w-screen sm:w-96 bg-gray-900 flex-col justify-center items-center text-center p-24'
                 key='menu'
                 initial={{ x: '100vh' }}
                 animate={{ x: 0 }}
@@ -40,11 +39,12 @@ const Sidebar = ({toggle, menuOpen }) => {
                     <h2 className='text-4xl'>Akari</h2>
                 </Link>
                 <hr className='my-4'/>
-                <ul className=''>
+                <ul className='gap-2 flex flex-col'>
                 <li><Link href="/" className="my-4 transition-all hover:text-yellow-200" passHref>HOME</Link></li>
                 <li><Link href="https://light-city.io" className="my-4 transition-all hover:text-yellow-200">STAKING</Link></li>
                 <li><Link href="https://lp.light-city.io" className="my-4 transition-all hover:text-sitePurple hover:bg-white px-4 py-2 border-[1px] rounded border-white" passHref>GLOW LP</Link></li>
                 <li><Link href="https://shrine.light-city.io/" className="my-4 transition-all hover:text-yellow-200" >THE SHRINE</Link></li>
+                <li><Link href="https://raffle.light-city.io/" className="my-4 transition-all hover:text-yellow-200" >RAFFLE DISTRICT</Link></li>
                     <li><hr className='my-4'/></li>
                     <li>
                         <div className='flex justify-center text-3xl'>
